ce68e65fbd9ed5174fb65e6e59c2a0908a0b4354
Dawid Jablonski <jablonskidawid0202@gmail.com>
2020-09-27 18:38:55 +0200

    Add UpdateCurrentViewModelCommand

---
+++ Transport.Notes.WPF/Commands/UpdateCurrentViewModelCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Transport.Notes.WPF.State.Navigators;
+using Transport.Notes.WPF.ViewModel.Factories;
+
+namespace Transport.Notes.WPF.Commands
+{
+    public class UpdateCurrentViewModelCommand : ICommand
+    {
+        private readonly INavigator _navigator;
+        private readonly ITransportNotesViewModelFacotry _viewModelFacotry;
+
+        public UpdateCurrentViewModelCommand(INavigator navigator, ITransportNotesViewModelFacotry viewModelFacotry)
+        {
+            _navigator = navigator;
+            _viewModelFacotry = viewModelFacotry;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            if(parameter is ViewType)
+            {
+                ViewType viewType = (ViewType)parameter;
+
+                _navigator.CurrentViewModel = _viewModelFacotry.CreateViewModel(viewType);
+            }
+        }
+    }
+}
