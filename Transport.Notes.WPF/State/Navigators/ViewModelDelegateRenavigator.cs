e518a85c1b94590844c68741c3489762ea51e4df
Dawid Jablonski <jablonskidawid0202@gmail.com>
2020-09-27 18:22:52 +0200

    Create Renavigator

---
+++ Transport.Notes.WPF/State/Navigators/ViewModelDelegateRenavigator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Transport.Notes.WPF.ViewModel;
+
+namespace Transport.Notes.WPF.State.Navigators
+{
+    public class ViewModelDelegateRenavigator<TViewModel> : IRenavigator where TViewModel : ViewModelBase
+    {
+        private readonly INavigator _navigator;
+        private readonly CreateViewModel<TViewModel> _createViewModel;
+
+        public ViewModelDelegateRenavigator(INavigator navigator, CreateViewModel<TViewModel> createViewModel)
+        {
+            _navigator = navigator;
+            _createViewModel = createViewModel;
+        }
+        public void Renavigator()
+        {
+            _navigator.CurrentViewModel = _createViewModel();
+        }
+    }
+}
