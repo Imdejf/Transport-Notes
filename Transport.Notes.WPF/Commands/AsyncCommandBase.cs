7ff5aaa9ba539c9772ec7ff9bdd75f09d8bd8458
Dawid Jablonski <jablonskidawid0202@gmail.com>
2020-10-10 14:56:01 +0200

    Add AsyncCommandBase

---
+++ Transport.Notes.WPF/Commands/AsyncCommandBase.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Transport.Notes.WPF.Commands
+{
+    public abstract class AsyncCommandBase : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        private bool _isExecuting;
+
+        public bool IsExecuting
+        {
+            get
+            {
+                return _isExecuting;
+            }
+            set
+            {
+                _isExecuting = value;
+                CanExecuteChanged?.Invoke(this,new EventArgs());
+            }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return !IsExecuting;
+        }
+
+        public async void Execute(object parameter)
+        {
+            IsExecuting = true;
+            await ExecuteAsync(parameter);
+            IsExecuting = false;
+        }
+
+        public abstract Task ExecuteAsync(object parameter);
+    }
+}
