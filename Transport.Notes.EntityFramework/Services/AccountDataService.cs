de4a8b30439ac677a6a4506195f3729d15d7e901
Dawid Jablonski <jablonskidawid0202@gmail.com>
2020-10-10 14:55:17 +0200

    Add Services in EntityFramework

---
+++ Transport.Notes.EntityFramework/Services/AccountDataService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Transport.Notes.Domain.Models;
+using Transport.Notes.Domain.Services;
+
+namespace Transport.Notes.EntityFramework.Services
+{
+    public class AccountDataService : IAccountService
+    {
+        public Task<Account> Create(Account entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Account> Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Account> GetByEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Account> GetByUsername(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Account> Update(Account entity, int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
