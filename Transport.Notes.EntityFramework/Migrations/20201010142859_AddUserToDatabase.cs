42fecb67f22bd8536a604a8286aa08d7b09005a5
Dawid Jablonski <jablonskidawid0202@gmail.com>
2020-10-10 16:29:45 +0200

    Create migration AddUserToDatabase

---
+++ Transport.Notes.EntityFramework/Migrations/20201010142859_AddUserToDatabase.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Transport.Notes.EntityFramework.Migrations
+{
+    public partial class AddUserToDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Accounts_User_AccountHolderId",
+                table: "Accounts");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_User",
+                table: "User");
+
+            migrationBuilder.RenameTable(
+                name: "User",
+                newName: "Users");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Users",
+                table: "Users",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Accounts_Users_AccountHolderId",
+                table: "Accounts",
+                column: "AccountHolderId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Accounts_Users_AccountHolderId",
+                table: "Accounts");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Users",
+                table: "Users");
+
+            migrationBuilder.RenameTable(
+                name: "Users",
+                newName: "User");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_User",
+                table: "User",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Accounts_User_AccountHolderId",
+                table: "Accounts",
+                column: "AccountHolderId",
+                principalTable: "User",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
