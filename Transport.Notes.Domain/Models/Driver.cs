1a8965dedd5834bf988568b39e3651ab8e85e97c
Dawid Jablonski <jablonskidawid0202@gmail.com>
2020-10-27 21:40:40 +0100

    Add Driver class

---
+++ Transport.Notes.Domain/Models/Driver.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Transport.Notes.Domain.Models
+{
+    public class Driver : DomainObject
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Notes { get; set; }
+        public DateTime EmploymentDay { get; set; }
+    }
+}
