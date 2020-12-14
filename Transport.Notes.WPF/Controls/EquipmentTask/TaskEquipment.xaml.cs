30e102773828e82b3c7f49c5241054db4a821d8f
Dawid Jablonski <jablonskidawid0202@gmail.com>
2020-12-14 18:10:38 +0100

    Create UserControl TaskEquipment

---
+++ Transport.Notes.WPF/Controls/EquipmentTask/TaskEquipment.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Transport.Notes.WPF.Controls.EquipmentTask
+{
+    /// <summary>
+    /// Logika interakcji dla klasy TaskEquipment.xaml
+    /// </summary>
+    public partial class TaskEquipment : UserControl
+    {
+        public TaskEquipment()
+        {
+            InitializeComponent();
+        }
+    }
+}
